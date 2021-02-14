28182d8227bd0ef6092f8b98f247d892c44b2268
Amirul Islam <amirulislamalmamun@gmail.com>
2021-02-14 14:38:25 +0600

    added sum of two number

---
+++ operation-new/add/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):    
+    
+    val1 = request.POST.get('num1')
+    val2 = request.POST.get('num2')
+    
+    sum = None
+    
+    if val1 and val2:
+        sum = int(val1) + int(val2)
+    
+    return render(request, 'home.html', {'val1': val1, 'val2': val2, 'result': sum})
