39e3438071f7d90723c5abc83c32be5a877538d8
Amirul Islam <amirulislamalmamun@gmail.com>
2021-03-28 17:48:19 +0600

    updated djrf project

---
+++ classroom/my_classroom/settings/__init__.py
@@ -0,0 +1,5 @@
+from my_classroom.settings.base import *
+from my_classroom.settings.apps import *
+from my_classroom.settings.templates import *
+from my_classroom.settings.database import *
+from my_classroom.settings.app_var import *
---
+++ classroom/my_classroom/settings/app_var.py
@@ -0,0 +1,41 @@
+ROOT_URLCONF = 'my_classroom.urls'
+
+WSGI_APPLICATION = 'my_classroom.wsgi.application'
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
